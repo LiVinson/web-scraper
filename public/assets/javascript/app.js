4a14d96c25e157c036ac18ab87b23e9ef8e4eb21
Lisa Vinson <Lrv0828@gmail.com>
2018-03-07 17:21:43 -0500

    added article and note schemas, put scrape logic inside of get request, pseudo code FE javascript

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,27 @@
+//On home page,
+    //GET request: articles, function(response) {
+        //do any necessary formatting
+        //render to handlebars
+    //}
+
+
+    //On click of article:
+        //GET body of article 
+        //Show comment box w/ article title
+        //Display any associated comments
+
+    //On click of submit (comments):
+        //Capture values and save
+        //POST reqeust (send comments)
+        //Display comments on the screen (optimistic UI)
+
+    //(While clicked on article) - On click of delete comment
+        //DELETE - comment that was clicked on (Optimistic UI - hide?)
+        //Refresh the articles page (to show comment was removed)
+
+    //On click of Scrape
+        //GET - New articles from reddit, function(response) {
+
+            //Redirect to home page (which displays all articles)
+        //}
+        
