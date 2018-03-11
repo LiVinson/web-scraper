581138bec986d5e4097214d1111b223d0ffea48a
Lisa Vinson <Lrv0828@gmail.com>
2018-03-11 16:01:37 -0400

    importing routes folder into server instead of individual file

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,12 @@
-require("./api_routes");
-require("./html_routes");
+module.exports = function () {
+    var router = require("express").Router();
+
+
+    var api_routes = require("./api_routes");
+    router.use("/", api_routes);
+
+    var html_routes = require("./html_routes");
+    router.use("/", html_routes);
+
+    return router;
+}
